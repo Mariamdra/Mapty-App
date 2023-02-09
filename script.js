890e957dc959f22bdcff1347adf3435f162f99f2
Mariama Drame <mariamadrame318@gmail.com>
2023-02-09 21:43:59 +0100

    displaying map marker

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@${latitude},${longitude},14z`);
 
-      const coords= [latitude, longitude]
+      const coords = [latitude, longitude];
 
       const map = L.map("map").setView(coords, 15);
 
@@ -39,13 +39,23 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-popup",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
     },
     function () {}
   );
-
-{
-}
